d5b847a0a7215f21ad41de28cb7d5b5b347c7380
HumairaRahaman <humairamitul@gmail.com>
2022-11-07 14:55:43 +0600

    teleport modal

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,14 @@ const router = createRouter({
       component: () => import('../views/PostsView.vue')
     },
     {
+      path: '/modals',
+      name: 'modals',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ModalsView.vue')
+    },
+    {
       path: '/postsDetails/:id',
       name: 'postsDetails',
       // route level code-splitting
---
+++ src/views/ModalsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button @click="showModal = true">Show Modal</button>
+    <teleport to=".modals-container">
+      <div v-if="showModal" class="modal">
+        <h1>This is my modal.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, sunt?</p>
+        <button @click="showModal = false">Hide Modal</button>
+      </div>
+    </teleport>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const showModal = ref(false);
+</script>
+
+<style scoped>
+.modal {
+  background: beige;
+  padding: 10px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 1;
+}
+</style>
