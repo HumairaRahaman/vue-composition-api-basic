120023cdd7e10f1eda1a5375b68f9516d1cb4303
HumairaRahaman <humairamitul@gmail.com>
2022-11-07 11:02:36 +0600

    local global custom directive

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,5 +2,10 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <textarea v-autofocus/>
   </div>
 </template>
+<script setup>
+import {vAutofocus} from '@/Derictive/vAutofocus'
+</script>
+
