4d90b6d67e94a37da63af2f82d8b59d0fdbf739a
HumairaRahaman <humairamitul@gmail.com>
2022-11-07 10:36:37 +0600

    mounted unmounted update multiple active hooks

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,11 @@
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-
+      <!-- <router-view v-slot="{ Component }">
+  <keep-alive>
+    <component :is="Component" />
+  </keep-alive>
+</router-view> -->
   <RouterView />
 </template>
 
